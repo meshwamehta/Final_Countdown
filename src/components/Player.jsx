539a0f353a84976b2e8f95645306002cb9c828a4
Meshwa Mehta <130814307+meshwamehta@users.noreply.github.com>
2023-12-28 19:50:39 -0500

    Update header Tag when you click SetName button
    before this update, Welcome header used to reflect the changes on every keystroke but using useState I updated to reflect change only when I click on SetName button.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 export default function Player() {
-  const [playerName,setName]=useState('Unknown entity');
-  function handleClick(e){
+  //state to see playername in <h2> only after clicking on button
+  const [submitted,setSubmitted]=useState(false);
+  const [playerName,setName]=useState('');
+  function handleChange(e){
+    //here I have to set submitted values to false because once I click setname 
+    //submitted will stay true and will change <h2> on every keystroke and to 
+    //avoid that I set it to false that means you can only see change name after clicking on setName button 
+    setSubmitted(false);
     setName(e.target.value);
+    
+  }
+  function handleSubmit(){
+      setSubmitted(true);
   }
   return (
     <section id="player">
-      <h2>Welcome {playerName}</h2>
+      {/*if button is clicked you will see the changed/entered name or else unkown entity displayed */}
+      <h2>Welcome {submitted ? playerName :"unknown entity"}</h2>
+      
       <p>
-        <input type="text" value={playerName} onChange={handleClick}/>
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={playerName} />
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
